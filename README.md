e25adb1a799bbfa3ff9dad7a567769229eac8075
Ismael Benito <ismael.benito.altamirano@gmail.com>
2021-02-07 17:02:07 +0100

    Add PyDev guide.

--- README.md
+++ README.md
@@ -80,4 +80,98 @@ One pip is updated let's grab at least IPython:
 $ python -m pip install ipython
 ```
 
+Now, from inside the plug-in Python Console, we can verify that we can import IPython: 
+
+```
+>>> import IPython
+>>> IPython.embed()
+Python 3.8.6 (default, Nov 10 2011, 15:00:00) 
+Type 'copyright', 'credits' or 'license' for more information
+IPython 7.20.0 -- An enhanced Interactive Python. Type '?' for help.
+
+In [1]: 
+```
+
+However, it will chrash inmediadtely. This is because the plug-in messes up with `sys.stdout` and `sys.sterr` objects, needed to be able to reproduce
+a 'console' application. So, there is possibility to improve the Python Console plug-in to become an IPython Console plug-in working on the
+Python code of this plug-in and adapt the way it manages those two objects.
+
+
+## Getting a minimal setup to develop code
+
+Having an IPython terminal is an advantatge over Python, because we have some extra instructions and access to shell, using `!`. However, what 
+we really want is to be able to run code externally from GIMP, to be able to rapidly prototype code, specially in Python. A possible solution 
+could be use a [PyDev server](https://www.pydev.org/) - client scheme. PyDev was originally developed to be used inside Eclipse, but also PyCharm
+has a port of its own. Where PyCharm or the IDE will act as a server for debugging and the app will act a client of that debugger. 
+
+So, let's do that. In PyCharm go to `Edit Configurations ... > + > Python Debug Server`, there you will see the instructions to run this scheme
+in a Python project. Mines are:
+
+- Install pydevd-pycharm corresponding to a current PyCharm version:
+
+```
+pip install pydevd-pycharm~=203.7148.72
+```
+
+- Run this two lines inside your application (a.k.a GIMP): 
+
+```python
+import pydevd_pycharm
+pydevd_pycharm.settrace('localhost', port=9000, stdoutToServer=True, stderrToServer=True)
+```
+
+If you try this two lines inside the plug-in Python Console you will end with the same problem than IPython, these two solutions rely on 
+`sys.stdout` and `sys.stderr`, but we are not giving up. So the main idea is to execute those two lines. So easy, let's hack into the plug-in
+files and put them. However, for the sake of the demo, let's use an even simplier plug-in, there is a bright new plug-ins templates for
+several languages in a new location `Filters > Development > Goat Exercices > Excercise a goat and a python`. Which file is located (from 
+the sandbox point of view) at `/app/lib/gimp/2.99/extensions/org.gimp.extension.goat-exercises/goat-exercise-py3.py`
+
+This plug-in is a simple 
+plug-in where the current loaded GIMP image (thus you need one opened), is color inverted. We are going to hook to our server using this plug-in.
+
+We can define a function to wrap the trace: 
+
+```python
+def set_trace(): return pydevd_pycharm.settrace('localhost', port=9000, stdoutToServer=True, stderrToServer=True)
+```
+
+So the question to ask now is where the hell I put this call. Obviously you want it to run when the plug-in starts, there is a line where the 
+main method is defined: 
+
+```python
+    def run(self, procedure, run_mode, image, drawable, args, run_data):
+```
+
+One can add the `set_trace()` call just there, however we will not know if it is actually running, and what is worse, we will freeze GIMP! So,
+we need one last leap of faith and multiprocessing, more multiprocessing than faith. So, we have to add this import: 
+
+```
+import multiprocessing as mp
+```
+
+And just before the `while (True)` loop begins, we can add:
+
+```python
+            th = mp.Process(target=set_trace)
+            th.start()
+```
+
+Before any return, this should be called `th.terminate()` to avoid another freeze. And it works! We are providing an isolated Python terminal
+to the debugger, which is running inside GIMP process, so it has access to GIMP bindings. My case: 
+
+![GIMP PyDev Demo](GIMP-PyDev-Demo.jpeg)
+
+## Modify the file
+
+There is a modified version of the plug-in in the repo, you can use it to copy it to the app folder. However, notice that you need to use you 
+actual environment and not the Sandbox, so, assuming that you are in the repo's root folder:
+
+```
+$ cp plug-ins/pydev/goat-exercise-py3.py ~/.local/share/flatpak/app/org.gimp.GIMP/current/active/files/lib/gimp/2.99/extensions/org.gimp.extension.goat-exercises
+```
+
+# TODO list
+
+- Fork Python Console plug-in to be able to run IPython.
+- Create a minimal Python plug-in to create a PyDev client. 
 
---
+++ plug-ins/pydev/goat-exercise-py3.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#    This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import gi
+gi.require_version('Gimp', '3.0')
+from gi.repository import Gimp
+gi.require_version('GimpUi', '3.0')
+from gi.repository import GimpUi
+gi.require_version('Gegl', '0.4')
+from gi.repository import Gegl
+from gi.repository import GObject
+from gi.repository import GLib
+from gi.repository import Gio
+
+import gettext
+import os
+import sys
+
+import pydevd_pycharm
+import multiprocessing as mp
+
+
+def set_trace(): return pydevd_pycharm.settrace('localhost', port=10002, stdoutToServer=True, stderrToServer=True)
+
+
+# Set-up localization for your plug-in with your own text domain.
+# This is complementary to the gimp_plug_in_set_translation_domain()
+# which is only useful for the menu entries inside GIMP interface,
+# whereas the below calls are used for localization within the plug-in.
+textdomain = 'gimp30-std-plug-ins'
+gettext.bindtextdomain(textdomain, Gimp.locale_directory())
+gettext.bind_textdomain_codeset(textdomain, 'UTF-8')
+gettext.textdomain(textdomain)
+_ = gettext.gettext
+def N_(message): return message
+
+class Goat (Gimp.PlugIn):
+    ## GimpPlugIn virtual methods ##
+    def do_query_procedures(self):
+        # Localization for the menu entries. It has to be called in the
+        # query function only.
+        self.set_translation_domain(textdomain,
+                                    Gio.file_new_for_path(Gimp.locale_directory()))
+
+        return [ "plug-in-goat-exercise-python" ]
+
+    def do_create_procedure(self, name):
+        procedure = Gimp.ImageProcedure.new(self, name,
+                                       Gimp.PDBProcType.PLUGIN,
+                                       self.run, None)
+
+        procedure.set_image_types("*")
+
+        procedure.set_menu_label(N_("Exercise a goat and a python"))
+        procedure.set_icon_name(GimpUi.ICON_GEGL)
+        procedure.add_menu_path('<Image>/Filters/Development/Goat exercises/')
+
+        procedure.set_documentation(N_("Exercise a goat in the Python 3 language"),
+                                    N_("Takes a goat for a walk in Python 3"),
+                                    name)
+        procedure.set_attribution("Jehan", "Jehan", "2019")
+
+        return procedure
+
+    def run(self, procedure, run_mode, image, drawable, args, run_data):
+
+        if run_mode == Gimp.RunMode.INTERACTIVE:
+            gi.require_version('Gtk', '3.0')
+            from gi.repository import Gtk
+            gi.require_version('Gdk', '3.0')
+            from gi.repository import Gdk
+
+            GimpUi.init("goat-exercise-py3.py")
+
+            dialog = GimpUi.Dialog(use_header_bar=True,
+                                   title=_("Exercise a goat (Python 3)"),
+                                   role="goat-exercise-Python3")
+
+            dialog.add_button(_("_Cancel"), Gtk.ResponseType.CANCEL)
+            dialog.add_button(_("_Source"), Gtk.ResponseType.APPLY)
+            dialog.add_button(_("_OK"), Gtk.ResponseType.OK)
+
+            geometry = Gdk.Geometry()
+            geometry.min_aspect = 0.5
+            geometry.max_aspect = 1.0
+            dialog.set_geometry_hints(None, geometry, Gdk.WindowHints.ASPECT)
+
+            box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=2)
+            dialog.get_content_area().add(box)
+            box.show()
+
+            # XXX We use printf-style string for sharing the localized
+            # string. You may just use recommended Python format() or
+            # any style you like in your plug-ins.
+            head_text=_("This plug-in is an exercise in '%s' to "
+                        "demo plug-in creation.\nCheck out the last "
+                        "version of the source code online by clicking "
+                        "the \"Source\" button.") % ("Python 3")
+            label = Gtk.Label(label=head_text)
+            box.pack_start(label, False, False, 1)
+            label.show()
+
+            contents = None
+            # Get the file contents Python-style instead of using
+            # GLib.file_get_contents() which returns bytes result, and
+            # when converting to string, get newlines as text contents.
+            # Rather than wasting time to figure this out, use Python
+            # core API!
+            with open(os.path.realpath(__file__), 'r') as f:
+                contents = f.read()
+
+            if contents is not None:
+                scrolled = Gtk.ScrolledWindow()
+                scrolled.set_vexpand (True)
+                box.pack_start(scrolled, True, True, 1)
+                scrolled.show()
+
+                view = Gtk.TextView()
+                view.set_wrap_mode(Gtk.WrapMode.WORD)
+                view.set_editable(False)
+                buffer = view.get_buffer()
+                buffer.set_text(contents, -1)
+                scrolled.add(view)
+                view.show()
+
+            th = mp.Process(target=set_trace)
+            th.start()
+
+            while (True):
+                response = dialog.run()
+                if response == Gtk.ResponseType.OK:
+                    dialog.destroy()
+                    break
+                elif response == Gtk.ResponseType.APPLY:
+                    url = "https://gitlab.gnome.org/GNOME/gimp/-/blob/master/extensions/goat-exercises/goat-exercise-py3.py"
+                    Gio.app_info_launch_default_for_uri(url, None)
+                    continue
+                else:
+                    th.terminate()
+                    dialog.destroy()
+                    return procedure.new_return_values(Gimp.PDBStatusType.CANCEL,
+                                                       GLib.Error())
+
+            th.terminate()
+        intersect, x, y, width, height = drawable.mask_intersect()
+        if intersect:
+            Gegl.init(None)
+
+            buffer = drawable.get_buffer()
+            shadow_buffer = drawable.get_shadow_buffer()
+
+            graph = Gegl.Node()
+            input = graph.create_child("gegl:buffer-source")
+            input.set_property("buffer", buffer)
+            invert = graph.create_child("gegl:invert")
+            output = graph.create_child("gegl:write-buffer")
+            output.set_property("buffer", shadow_buffer)
+            input.link(invert)
+            invert.link(output)
+            output.process()
+
+            # This is extremely important in bindings, since we don't
+            # unref buffers. If we don't explicitly flush a buffer, we
+            # may left hanging forever. This step is usually done
+            # during an unref().
+            shadow_buffer.flush()
+
+            drawable.merge_shadow(True)
+            drawable.update(x, y, width, height)
+            Gimp.displays_flush()
+
+        return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
+
+Gimp.main(Goat.__gtype__, sys.argv)
