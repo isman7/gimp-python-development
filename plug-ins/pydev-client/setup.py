aad6395c6bc08dd0973e8b0669d41c820a6087a3
Ismael Benito <ismael.benito.altamirano@gmail.com>
2021-05-29 00:33:01 +0200

    Better package structures.

--- plug-ins/pydev-client/setup.py
+++ plug-ins/pydev-client/setup.py
@@ -5,14 +5,14 @@ from setuptools import find_packages
 requirements = Path("requirements.txt").read_text().split("\n")
 
 setup(
-    name='gimp-console',
+    name='gimp-pydev-pycharm',
     version='0.1.0',
     packages=find_packages(),
     url='https://github.com/isman7/gimp-python-development/',
     license='GNU GPLv3',
     author='ibenito',
     author_email='ismaelbenito@protonmail.com',
-    description='A GTK Python Console for the GIMP python environment',
+    description='A PyDev for PyCharm debugging inside the GIMP python environment',
     install_requires=requirements,
-    package_data={"gimp.plugins.console": ["console"]}
+    package_data={"gimp.plugins.pydev": ["pydev"]}
 )
